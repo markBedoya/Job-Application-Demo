7d2b23db139bcc3df4e839ed6aa9295c5671961a
Mark Bedoya <mrkbedoya@gmail.com>
2024-04-17 16:06:23 -0500

    refactor

--- src/main/java/org/example/jobapplicationdemo/JobAppApplication.java
+++ src/main/java/org/example/jobapplicationdemo/JobAppApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobApplicationDemoApplication {
+public class JobAppApplication {
 
   //TODO - impl test coverage of expected results. Complete integration / unit tests / best practices
   //TODO - Best practices to document API functionality and include in project
 
   public static void main(String[] args) {
-    SpringApplication.run(JobApplicationDemoApplication.class, args);
+    SpringApplication.run(JobAppApplication.class, args);
   }
 
 }
