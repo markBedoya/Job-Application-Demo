a4ae09aa013a3cdd1f0eff2ba0e2d2ed93cffb1d
Mark Bedoya <mrkbedoya@gmail.com>
2024-04-30 19:05:33 -0500

    removing comments

--- src/main/java/org/example/jobapplicationdemo/JobAppApplication.java
+++ src/main/java/org/example/jobapplicationdemo/JobAppApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobAppApplication {
 
-  //TODO - impl test coverage of expected results. Complete integration / unit tests / best practices
-  //TODO - Best practices to document API functionality and include in project
-
   public static void main(String[] args) {
     SpringApplication.run(JobAppApplication.class, args);
   }
